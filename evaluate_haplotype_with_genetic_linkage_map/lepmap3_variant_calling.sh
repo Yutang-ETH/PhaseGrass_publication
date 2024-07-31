a489c38d28ea298906710d03b1ddc9450e110228
Yutang-ETH <84848653+Yutang-ETH@users.noreply.github.com>
2024-07-31 17:15:13 +0200

    Add files via upload

---
+++ evaluate_haplotype_with_genetic_linkage_map/lepmap3_variant_calling.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# try lep_map3 variant calling pipeline
+mypath=/home/yutachen/public/Yutangchen/Lepmap3_20240208/bin
+
+# prepare the sorted_bams file
+# sed 's/^/map\//' new_sample_order | sed 's/$/.bam/' | tr '\n' '\t' > sorted_bams
+
+# prepare the mapping.txt file
+# cat new_sample_order > mapping.txt
+
+# run variant calling in parallel
+for i in chr1_h2 chr2_h2 chr3_h2 chr4_h2 chr5_h2 chr6_h2 chr7_h2
+do
+    echo "samtools mpileup -r \"$i\" -q 10 -Q 10 -s \$(cat sorted_bams) | java -cp $mypath Pileup2Likelihoods > \"$i\".post"
+done >SNP_calling.txt
+
+# run samtools mpileup in parallel
+parallel --jobs 48 < SNP_calling.txt
+
+# cat all the post files
+cat *.post | awk '(NR==1 || ($1!="CHR"))' | gzip > all_post.gz

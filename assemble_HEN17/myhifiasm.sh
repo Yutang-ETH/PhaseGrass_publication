c137731257c20d5c2a88c8a68f34eac4db5654a7
Yutang-ETH <84848653+Yutang-ETH@users.noreply.github.com>
2024-08-01 01:39:16 +0200

    Add files via upload

---
+++ assemble_HEN17/myhifiasm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# hifiasm -o hifiasm/redclover.asm -t 48 -u 0 --h1 HIC/SRR15433790_1.fastq,HIC/SRR15433791_1.fastq,HIC/SRR15433792_1.fastq --h2 HIC/SRR15433790_2.fastq,HIC/SRR15433791_2.fastq,HIC/SRR15433792_2.fastq HIFI/SRR15433789.fastq
+
+# awk '/^S/{print ">"$2;print $3}' hifiasm/redclover.asm.hic.hap1.p_ctg.gfa > hifiasm/redclover.hap1.ctg.fa
+# awk '/^S/{print ">"$2;print $3}' hifiasm/redclover.asm.hic.hap2.p_ctg.gfa > hifiasm/redclover.hap2.ctg.fa
+# awk '/^S/{print ">"$2;print $3}' hifiasm/redclover.asm.hic.p_utg.gfa > hifiasm/redclover.utg.fa
+
+assembly-stats -t hifiasm/redclover.hap1.ctg.fa hifiasm/redclover.hap2.ctg.fa hifiasm/redclover.utg.fa > hifiasm/hifiasm.asm.stats 
